dd2c42d8849fcc8b513b876e8613d0e40cfbc49d
Seno Pamungkas <senop.rahman@gmail.com>
2026-03-16 10:25:29 +0700

    initial golang project

--- cmd/autocv/main.go
+++ cmd/autocv/main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"github.com/cattyman919/autocv/internal/ui"
-)
-
 func main() {
-	app := ui.NewApp()
-
-	if err := app.Run(); err != nil {
-		panic(err)
-	}
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/cattyman919/autocv
 
-go 1.25.7
+go 1.26
 
 require (
 	github.com/gdamore/encoding v1.0.1 // indirect
